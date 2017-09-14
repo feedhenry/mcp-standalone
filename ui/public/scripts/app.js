b7d1a81af170fd8e21ecd95b12d284b1a23177fc
Phil Brookes <phil.brookes@redhat.com>
2017-09-14 08:43:52 +0100

    remove MCP_URL caching

--- ui/public/scripts/app.js
+++ ui/public/scripts/app.js
@@ -43,7 +43,12 @@ var resolveMCPRoute = {
     'ProjectsService',
     'DataService',
     function($route, ProjectsService, DataService) {
-      if (window.MCP_URL) {
+      // this is reset to null so that it is not persisted from a request in one namespace
+      // to a request in a different namespace
+      window.MCP_URL = null;
+      //GLOBAL_MCP_URL is used either for local development or a global MCP server
+      if (typeof window.GLOBAL_MCP_URL !== 'undefined') {
+        window.MCP_URL = window.GLOBAL_MCP_URL;
         return;
       }
       return ProjectsService.get($route.current.params.project).then(
