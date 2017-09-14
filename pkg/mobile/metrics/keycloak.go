017b239589d7a2de31d93cd665680422c812fda9
craig <cbrookes@redhat.com>
2017-09-14 11:31:56 +0100

    add initial metrics endpoint

--- pkg/mobile/metrics/keycloak.go
+++ pkg/mobile/metrics/keycloak.go
@@ -6,6 +6,8 @@ type Keycloak struct{}
 
 // Gather will retrieve varous metrics from keycloak
 func (kc *Keycloak) Gather() ([]*metric, error) {
-	m := &metric{Type: "logins", XValue: time.Now().String(), YValue: 10}
+	now := time.Now()
+
+	m := &metric{Type: "logins", XValue: now.Format(time.RFC3339), YValue: 10}
 	return []*metric{m}, nil
 }
