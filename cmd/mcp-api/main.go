0cd91b07d18ebcf399ee699c403a50975f2a4c55
craig <cbrookes@redhat.com>
2017-09-22 16:44:39 +0100

    rename pkg to make it clearer what it is

--- cmd/mcp-api/main.go
+++ cmd/mcp-api/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/feedhenry/mcp-standalone/pkg/clients"
+	"github.com/feedhenry/mcp-standalone/pkg/httpclient"
 	"github.com/feedhenry/mcp-standalone/pkg/data"
 	"github.com/feedhenry/mcp-standalone/pkg/k8s"
 	"github.com/feedhenry/mcp-standalone/pkg/mobile"
@@ -69,7 +69,7 @@ func main() {
 		svcRepoBuilder     = data.NewServiceRepoBuilder(k8ClientBuilder, *namespace, token)
 		authCheckerBuilder = openshift.NewAuthCheckerBuilder(k8host)
 		userRepoBuilder    = openshift.NewUserRepoBuilder(k8host, true).WithClient(&openshift.UserAccess{})
-		httpClientBuilder  = clients.NewHttpClientBuilder()
+		httpClientBuilder  = httpclient.NewHttpClientBuilder()
 		openshiftUser      = openshift.UserAccess{}
 		mwAccess           = middleware.NewAccess(logger, k8host, openshiftUser.ReadUserFromToken)
 		// these channels control when background proccess should stop
