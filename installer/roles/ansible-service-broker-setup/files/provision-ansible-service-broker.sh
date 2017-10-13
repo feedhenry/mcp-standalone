febf67285b60edf0cee9521de30d4688ae854582
David Martin <david.martin@redhat.com>
2017-10-13 12:53:30 +0100

    Allow passing the routing_suffix to the ansible service broker installer

--- installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
+++ installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
@@ -4,6 +4,7 @@ readonly DOCKERHUB_USER="${1}"
 readonly DOCKERHUB_PASS="${2}"
 readonly DOCKERHUB_ORG="${3}"
 readonly LAUNCH_APB_ON_BIND="${4}"
+readonly ROUTING_SUFFIX="${5}"
 curl -s https://raw.githubusercontent.com/openshift/ansible-service-broker/d93206936751437cc7edaa15a2c88ef317d4c698/templates/deploy-ansible-service-broker.template.yaml > /tmp/deploy-ansible-service-broker.template.yaml
 
 oc login -u system:admin
@@ -16,7 +17,7 @@ oc process -f /tmp/deploy-ansible-service-broker.template.yaml \
     -p BROKER_IMAGE="ansibleplaybookbundle/origin-ansible-service-broker:sprint137" \
     -p ENABLE_BASIC_AUTH="false" \
     -p SANDBOX_ROLE="admin" \
-    -p ROUTING_SUFFIX="192.168.37.1.nip.io" \
+    -p ROUTING_SUFFIX="${ROUTING_SUFFIX}" \
     -p LAUNCH_APB_ON_BIND="${LAUNCH_APB_ON_BIND}" | oc create -f -
 
 if [ "${?}" -ne 0 ]; then
