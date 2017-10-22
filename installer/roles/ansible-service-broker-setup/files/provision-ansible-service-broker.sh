73af060e5ff3a8a10608182ca411b89ace5dc79b
craig <cbrookes@redhat.com>
2017-10-22 12:04:11 +0100

    initial release doc and impl

--- installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
+++ installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+set -e
 
 readonly DOCKERHUB_USER="${1}"
 readonly DOCKERHUB_PASS="${2}"
 readonly DOCKERHUB_ORG="${3}"
 readonly LAUNCH_APB_ON_BIND="${4}"
+readonly TAG="${5}"
+
+echo "starting install of ansible service broker"
+
+function finish {
+  echo "unexpected exit of ansible service broker installation script"
+}
+
+trap 'finish' EXIT
+
 curl -s https://raw.githubusercontent.com/openshift/ansible-service-broker/d93206936751437cc7edaa15a2c88ef317d4c698/templates/deploy-ansible-service-broker.template.yaml > /tmp/deploy-ansible-service-broker.template.yaml
 
 oc login -u system:admin
@@ -17,6 +28,7 @@ oc process -f /tmp/deploy-ansible-service-broker.template.yaml \
     -p ENABLE_BASIC_AUTH="false" \
     -p SANDBOX_ROLE="admin" \
     -p ROUTING_SUFFIX="192.168.37.1.nip.io" \
+    -p TAG="${TAG:-latest}" \
     -p LAUNCH_APB_ON_BIND="${LAUNCH_APB_ON_BIND}" | oc create -f -
 
 if [ "${?}" -ne 0 ]; then
