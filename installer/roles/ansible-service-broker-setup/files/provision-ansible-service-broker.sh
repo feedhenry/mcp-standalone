7b05164cc992722de8c83bfb377ac76d485f6cef
Aiden Keating <akeating@redhat.com>
2017-10-23 12:56:38 +0100

    3.7

--- installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
+++ installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
@@ -3,7 +3,7 @@ set -e
 
 readonly DOCKERHUB_USER="${1}"
 readonly DOCKERHUB_PASS="${2}"
-readonly DOCKERHUB_ORG="${3}"
+readonly DOCKERHUB_ORG="aidenkeatingrht"
 readonly LAUNCH_APB_ON_BIND="${4}"
 readonly TAG="${5}"
 
@@ -15,21 +15,25 @@ function finish {
 
 trap 'finish' EXIT
 
-curl -s https://raw.githubusercontent.com/openshift/ansible-service-broker/d93206936751437cc7edaa15a2c88ef317d4c698/templates/deploy-ansible-service-broker.template.yaml > /tmp/deploy-ansible-service-broker.template.yaml
+readonly TEMPLATE_URL="https://raw.githubusercontent.com/openshift/ansible-service-broker/master/templates/deploy-ansible-service-broker.template.yaml"
+readonly TEMPLATE_VARS="-p BROKER_CA_CERT=$(oc get secret -n kube-service-catalog -o go-template='{{ range .items }}{{ if eq .type "kubernetes.io/service-account-token" }}{{ index .data "service-ca.crt" }}{{end}}{{"\n"}}{{end}}' | tail -n 1)"
+
 
 oc login -u system:admin
 oc new-project ansible-service-broker
-oc process -f /tmp/deploy-ansible-service-broker.template.yaml \
+curl -s ${TEMPLATE_URL} \
+    | oc process -f /tmp/deploy-ansible-service-broker.template.yaml \
     -n ansible-service-broker \
     -p DOCKERHUB_USER="${DOCKERHUB_USER}" \
     -p DOCKERHUB_PASS="${DOCKERHUB_PASS}" \
     -p DOCKERHUB_ORG="${DOCKERHUB_ORG}" \
-    -p BROKER_IMAGE="ansibleplaybookbundle/origin-ansible-service-broker:sprint137" \
+    -p BROKER_IMAGE="ansibleplaybookbundle/origin-ansible-service-broker:latest" \
     -p ENABLE_BASIC_AUTH="false" \
     -p SANDBOX_ROLE="admin" \
     -p ROUTING_SUFFIX="192.168.37.1.nip.io" \
     -p TAG="${TAG:-latest}" \
-    -p LAUNCH_APB_ON_BIND="${LAUNCH_APB_ON_BIND}" | oc create -f -
+    -p LAUNCH_APB_ON_BIND="${LAUNCH_APB_ON_BIND}" \
+    ${TEMPLATE_VARS} | oc create -f -
 
 if [ "${?}" -ne 0 ]; then
 	echo "Error processing template and creating deployment"
